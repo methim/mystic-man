ea86b9b70d722c584855fd89529ce33c82b462d7
methim <80419860+methim@users.noreply.github.com>
2021-03-14 13:42:18 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,36 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 4 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 d f d d d f d 4 . . . . 
+. . . 4 d f d d d f d 4 . . . . 
+. . . 4 d f d d d f d 4 . . . . 
+. . . 4 d d d d d d d 4 . . . . 
+. . . 4 d d f f f d d 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
